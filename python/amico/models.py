b5f8ce337dc226c7121b67024957770df090f6b5
David Romascano <david.romascano@epfl.ch>
2016-05-31 09:30:08 +0200

    RF: splitted ADD computation in several steps

--- python/amico/models.py
+++ python/amico/models.py
@@ -434,8 +434,9 @@ class CylinderZeppelinBall( BaseModel ) :
         v = f1 / ( f1 + f2 + 1e-16 )
         xIC = x[:nD*n1].reshape(-1,n1).sum(axis=0)
         a = 1E6 * 2.0 * np.dot(self.Rs,xIC) / ( f1 + 1e-16 )
-        nwADD = xIC / (2.*self.Rs)**2 # [Benjamini et al., "White matter microstructure from nonparametric axon diameter distribution mapping", Neuroimage 2016]
-        nwa = 1E6 * 2.0 * np.dot(self.Rs,nwADD) / ( nwADD.sum() + 1e-16 )
+        nwADD = (xIC/xIC.sum()) * (1 / (2E6*self.Rs)**2) # [Benjamini et al., "White matter microstructure from nonparametric axon diameter distribution mapping", Neuroimage 2016]
+        nwADD = nwADD / ( nwADD.sum() + 1e-16 )
+        nwa = 2E6 * np.dot(self.Rs,nwADD)
         d = (4.0*v) / ( np.pi*a**2 + 1e-16 )
         return [v, a, d, nwa], dirs, x, A
 
@@ -618,7 +619,8 @@ class CylinderTimedepZeppelinBall( BaseModel ) :
         v = f1 / ( f1 + f2 + 1e-16 )
         xIC = x[:nD*n1].reshape(-1,n1).sum(axis=0)
         a = 1E6 * 2.0 * np.dot(self.Rs,xIC) / ( f1 + 1e-16 )
-        nwADD = xIC / (2.*self.Rs)**2 # [Benjamini et al., "White matter microstructure from nonparametric axon diameter distribution mapping", Neuroimage 2016]
+        nwADD = (xIC/xIC.sum()) * (1 / (2.*self.Rs)**2) # [Benjamini et al., "White matter microstructure from nonparametric axon diameter distribution mapping", Neuroimage 2016]
+        nwADD = nwADD / nwADD.sum()
         nwa = 1E6 * 2.0 * np.dot(self.Rs,nwADD) / ( nwADD.sum() + 1e-16 )
         d = (4.0*v) / ( np.pi*a**2 + 1e-16 )
         xEC = x[nD*n1:nD*(n1+n2)].reshape(-1,n2).sum(axis=0)
